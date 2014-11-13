47beff1697df264355c08897246646a6d81191c7
Kurt DiPaolo <kurtdipaolo@gmail.com>
2014-11-12 19:00:45 -0500

    Boolean Lesson

--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -1,19 +1,19 @@
 module.exports.equalStrings = function(stringOne, stringTwo) {
-
+return stringOne == stringTwo; 
 };
 
 module.exports.notEqual = function(one, two) {
-
+return one !== two; 
 };
 
 module.exports.inBetween = function(lower, middle, upper) {
-
+return (lower < middle) && (upper > middle);
 };
 
 module.exports.outsideRanges = function(number) {
-
+return !(10 <= number && number <= 20) && !(42 < number && number <= 75) &&  !(6 < number && number < 1);
 };
 
 module.exports.nameAndPrice = function(name, price) {
-
+return (name == 'NYTimes' || name == 'LATimes') && (price >= 1);
 };
