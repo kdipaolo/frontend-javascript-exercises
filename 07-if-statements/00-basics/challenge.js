b0035692e01e5e16a5f95c7b403d25a7ac2fef36
Kurt DiPaolo <kurtdipaolo@gmail.com>
2014-11-14 13:42:40 -0500

    IF Else Lesson

--- 07-if-statements/00-basics/challenge.js
+++ 07-if-statements/00-basics/challenge.js
@@ -1,5 +1,40 @@
-module.exports.favoriteNumber = undefined;
+module.exports.favoriteNumber = function(fav,guess){
 
-module.exports.checkLock = undefined;
+if(guess > fav){
+  return "Too high"
+}else if(guess < fav){
+  return "Too low"
+}
+else if(guess === fav){
+  return "You got it!"
+}
+};
 
-module.exports.canIGet = undefined;
+module.exports.checkLock = function(a,b,c,d){
+  if( (a === 3 || a === 5 || a === 7) && (b === 2) && (c >= 5 && c <= 100)  && (d < 9 || d > 20) ){
+    return "correct"
+  }else {
+    return "incorrect"
+  }
+};
+
+
+module.exports.canIGet = function(item,money){
+  if (item === 'MacBook Air' && money >= 999){
+    return true
+  }
+  else if (item === 'MacBook Pro' && money >= 1299){
+    return true
+  }
+  else if (item === 'Mac Pro' && money >= 2499){
+    return true;
+  }
+  else if (item === 'Apple Sticker' && money >= 1){
+    return true;
+  } 
+
+  else {
+    return false;
+  }
+}
+
