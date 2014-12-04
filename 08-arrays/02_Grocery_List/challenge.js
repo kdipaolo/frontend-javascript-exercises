e1f222b734d2546cece2e19951c0cbca7f0a99a2
kdipaoloe <kurtdipaolo@gmail.com>
2014-12-04 16:44:27 -0500

    Arrays Checkpoint

--- 08-arrays/02_Grocery_List/challenge.js
+++ 08-arrays/02_Grocery_List/challenge.js
@@ -1,3 +1,28 @@
-module.exports.addItem = undefined;
+module.exports.addItem = function(newItem, list){
 
-module.exports.reverseSortedList = undefined;
+var lastElementIndex = list.length - 1; 
+
+list.push(newItem);
+return list;
+
+};
+// Create an addItem function. This function should take two arguments:
+
+// the first argument is the item you want to add to the end of the list
+// the second argument is the array that the item will be added to.
+// Don't add an item to the list if it already exists in the list
+
+module.exports.reverseSortedList = function(array){
+  var sort = array.sort();
+  return sort.reverse();
+};
+
+// module.exports.addItem = function(newItem, list){
+
+// var lastElementIndex = list.length - 1; 
+
+// if(list[lastElementIndex] != newItem){
+// list.push(newItem);
+// return list;
+
+// };
